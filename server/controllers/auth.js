73b362678a3cccd62a87121b879681851ddc7b88
leomarker <85751407+leomarker@users.noreply.github.com>
2022-12-29 13:55:15 +0300

    #

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,2 +1,22 @@
 import bcrypt from "bcrypt";
 import { Jwt } from "jsonwebtoken";
+import User from "../model/user";
+
+export const register = async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    picturePath,
+    friends,
+    location,
+    occupation,
+  } = req.body;
+
+  const user = User.findOne({ email: email });
+  if (!user) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+  }
+};
