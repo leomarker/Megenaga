cd1b38bf94b114e8072e07bd14e7ad232d2ffbdd
leomarker <85751407+leomarker@users.noreply.github.com>
2022-12-29 16:19:38 +0300

    #

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,8 @@
-import bcrypt from "bcrypt";
-import { Jwt } from "jsonwebtoken";
-import User from "../model/user";
+const bcrypt = require("bcrypt");
+const { Jwt } = require("jsonwebtoken");
+const User = require("../model/user");
 
-export const register = async (req, res, next) => {
+exports.register = async (req, res, next) => {
   const {
     firstName,
     lastName,
@@ -14,9 +14,26 @@ export const register = async (req, res, next) => {
     occupation,
   } = req.body;
 
-  const user = User.findOne({ email: email });
-  if (!user) {
+  const userExist = await User.findOne({ email: email });
+  if (!userExist) {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
+    let user = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    });
+    await user.save();
+    user.password = undefined;
+
+    console.log(user);
+    res.status(201).json(user);
+  } else {
+    res.status(500).json({ error: "user exists" });
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,44 +1,41 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dontEnv from "dotenv";
-import multer from "multer";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import helmet from "helmet";
-
-import {register} form "./controllers/auth.js";
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dontEnv = require("dotenv");
+const multer = require("multer");
+const morgan = require("morgan");
+const path = require("path");
+const { fileURLToPath } = require("url");
+const helmet = require("helmet");
+const { register } = require("./controllers/auth");
 /* config */
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const filename = fileURLToPath();
+// const dirname = path.dirname(filename);
 dontEnv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
 // app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-orgin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+// app.use(morgan("common"));
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use("/assets", express.static(path.join(dirname, "public/assets")));
 
 /*storage*/
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/assets");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-app.use("/api/register/", upload.single("picture"), register);
+app.use("/api/register/", register);
 /* mongoose connection and server start*/
 const PORT = 5000;
 
