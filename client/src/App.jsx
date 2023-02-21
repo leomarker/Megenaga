8df39a703a8a0546d3729f393c4257883d6921e0
Nati <natnael3370@gmail.com>
2023-02-21 15:20:20 +0300

    setup for redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "/scenes/homepage";
-import LoginPage from "/scenes/loginpage";
-import ProfilePage from "/scenes/profilepage";
+import HomePage from "./scenes/homepage";
+import LoginPage from "./scenes/loginpage";
+import ProfilePage from "./scenes/profilepage";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { useSelector } from "react-redux";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />}></Route>
-          <Route path="/home" element={<HomePage />}></Route>
-          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route path="/home" element={<HomePage />}></Route>
+            <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
