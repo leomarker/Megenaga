eb6158441542abf1f3f66ac048ce8a00fbec2c46
Nati <natnael3370@gmail.com>
2023-03-07 09:32:13 +0300

    new home componets

--- client/src/scenes/homepage/index.jsx
+++ client/src/scenes/homepage/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import { useMediaQuery } from "@chakra-ui/react";
 import NavBar from "../navbar/index";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return (
-    <div>
+    <Box bg="background.default" height="full">
       <NavBar />
-    </div>
+      <Box
+        width="100%"
+        padding="2rem"
+        display={isNonMobileScreens[0] ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Box>
+    </Box>
   );
 };
 
