17dcb2261004ba9c5b8bc609519d3b81f1bdea1b
Nati <natnael3370@gmail.com>
2023-03-07 14:45:27 +0300

    User profile component add

--- client/src/scenes/widgets/UserWidgets.jsx
+++ client/src/scenes/widgets/UserWidgets.jsx
@@ -8,13 +8,50 @@ import {
   LocationOnOutlined,
   WorkOutlineOutlined,
 } from "@mui/icons-material";
-import { Box, Text, Divider } from "@chakra-ui/react";
+import { Box, Text, Divider, Flex } from "@chakra-ui/react";
 
-const UserWidgets = ({ _id, picturePath }) => {
+const UserWidgets = ({ userId, picturePath }) => {
   const navigate = useNavigate();
   const token = useSelector((state) => state.token);
   const [user, setUser] = useState(null);
-  return <></>;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:8080/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+  if (!user) {
+    return null;
+  }
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <Box
+      padding={"1.5rem 1.5rem 0.75rem 1.5rem"}
+      bg="background.alt"
+      borderRadius="lg"
+    >
+      <Flex pb="1.1rem">
+        <Flex></Flex>
+        <ManageAccountsOutlined />
+      </Flex>
+    </Box>
+  );
 };
 
 export default UserWidgets;
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,9 +3,10 @@ const User = require("../model/user");
 exports.getUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await User.findOne(id);
+    const user = await User.findById(id);
     res.status(200).json({ user });
   } catch (err) {
+    console.log(err.message);
     res.status(500).json({ msg: err.message });
   }
 };
